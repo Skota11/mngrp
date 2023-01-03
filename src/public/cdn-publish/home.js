8aeafe490139a0e441124acd00856fc3078a5fd0
S Kota <91359399+Skota11@users.noreply.github.com>
2023-01-03 15:35:00 +0900

    nologin

--- src/public/cdn-publish/home.js
+++ src/public/cdn-publish/home.js
@@ -99,3 +99,17 @@ function reloadlogin() {
         })
     })
 }
+//Cookieを取得
+function getcookie(name) {
+    var cookies = document.cookie; //全てのcookieを取り出して
+    var cookiesArray = cookies.split(';'); // ;で分割し配列に
+    for (var c of cookiesArray) { //一つ一つ取り出して
+      var cArray = c.split('='); //さらに=で分割して配列に
+      if (cArray[0] == name) { // 取り出したいkeyと合致したら
+        return cArray[1]  // [key,value] 
+      }
+    }
+  }
+  if (getcookie("ac_") == null) {
+    location.href = "/login?q="
+  }
